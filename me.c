d5f3148e6a044d060533fe65993386c51680f06c
Derpimort <jatinsaini580@gmail.com>
2021-08-29 11:16:26 +0530

    ADD: simple c bio

---
+++ me.c
@@ -0,0 +1,21 @@
+#include "bio.h"
+#include "coding.h"
+#include "awesomeness.h"
+//#include "nobugs.h" /* Been giving a lot of problems lately */
+
+int main(){
+
+    char name[]         = "Jatin Saini";
+
+    char *langs[5]      = {"Python", "C", "Java", "Kotlin", "SQL"};
+
+    char *techs[7]      = {"Android", "Machine Learning", "Docker", "ELK", "AWS", "HPC", "Git"};
+
+    char *learning[4]   = {"Golang", "Flutter", "Django", "React"};
+
+    char *workingOn[2]  = {"Meme-Indexer", "This Bio"};
+
+    char *likes[5]      = {"Programming", "FPS Games", "Harmonica", "Minecraft"};
+
+    return 42;
+}
